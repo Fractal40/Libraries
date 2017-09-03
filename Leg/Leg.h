05cf2627afe8da7056dc7a7b8b85d20f47c52a65
Fractal40 <31180308+Fractal40@users.noreply.github.com>
2017-09-03 21:33:44 +0200

    fix
    relative servo angles - alla servos arrive at the same time(ish)

--- Leg/Leg.h
+++ Leg/Leg.h
@@ -15,22 +15,25 @@ class Leg
     void calcLegIk(int x, int y, int z);
     void setLegTiming(int updateInterval, int increment);
     int updateLeg();
+    void legOffset(int xOffset, int yOffset, int zOffset);
+
+  private:
     Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
     int pulse[3], lastPulse[3], servoNr[3];
-  private:
     int _coxaAngle, _femurAngle, _tibiaAngle;
     int _servoNrCoxa, _servoNrFemur, _servoNrTibia;
     int _x, _y, _z;
-    int _updateInterval=50, _increment=1;
+    int _xOffset, _yOffset, _zOffset;
+    int _updateInterval=500, _increment=1;
     int coxaPulse, femurPulse, tibiaPulse, coxaPulseLastpos, femurPulseLastpos, tibiaPulseLastpos;
-    
+    int stepFlag = 1;
     int _coxaLen, _femurLen, _tibiaLen; // lengths of leg parts - coxa, femur, tibia
     int const SERVOMINIMUM = 100, SERVOMAXIMUM = 600;
     unsigned long lastUpdate; // last update of position
 	int const SERVOMIN[12] = {239,238,262,252,256,256,234,258,234,252,242,252};
 	int const SERVOMAX[12] = {453,460,470,468,486,486,446,480,470,462,462,466};
 
-    
+
 };
 
 #endif
