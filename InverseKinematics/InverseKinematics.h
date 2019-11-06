1584c9b9049d83172766babfdea0aa820d5afe0b
Fractal40 <31180308+Fractal40@users.noreply.github.com>
2019-11-06 22:49:53 +0100

    ik
    new untested inverse kinematics library

---
+++ InverseKinematics/InverseKinematics.h
@@ -0,0 +1,27 @@
+#ifndef InverseKinematics_h
+#define InverseKinematics_h
+
+#include "Arduino.h"
+#include <avr/dtostrf.h>
+
+
+
+class InverseKinematics
+{
+	public:
+		InverseKinematics(int _limbA_Len, int _limbB_Len, int _limbC_Len);
+    void ik(int _x, int _y, int _z);
+    int getAngleDeg(int _jointNum);
+    int getAnglePulse(int _jointNum);
+
+
+	private:
+    void degToPulse();
+    int limbA_Len, limbB_Len, limbC_Len;
+    int joint[3];
+    int x, y, z;
+
+};
+
+
+#endif
